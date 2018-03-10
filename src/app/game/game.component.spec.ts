8090cbf0fc4e30e88cd7dfc21570e15d7eac8f9c
Taylor, Robert <Robert.Taylor@wolterskluwer.com>
2018-03-10 12:09:42 -0500

    improved dynamic buttons in game component

--- src/app/game/game.component.spec.ts
+++ src/app/game/game.component.spec.ts
@@ -2,10 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameComponent } from './game.component';
 import { FormsModule } from '@angular/forms';
-import { StraightPoolRulesService } from '../straight-pool-rules.service';
+import { StraightPoolRulesService, EndingType } from '../straight-pool-rules.service';
 import { PlayerComponent } from '../player/player.component';
+import { By } from '@angular/platform-browser';
 
-describe('GameComponent', () => {
+fdescribe('GameComponent', () => {
   let component: GameComponent;
   let fixture: ComponentFixture<GameComponent>;
 
@@ -31,4 +32,29 @@ describe('GameComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  fit('hides breaking foul when any balls are made', async(() => {
+    component.ballsRemaining = 14;
+    fixture.detectChanges();
+    const el: HTMLElement = fixture.nativeElement;
+    const breakingFoulButton = el.querySelector('button#breakingFoul');
+    expect(breakingFoulButton).toBeNull();
+  }));
+
+  fit('hides switch players when any balls are made', async(() => {
+    component.ballsRemaining = 14;
+    fixture.detectChanges();
+    const el: HTMLElement = fixture.nativeElement;
+    const breakingFoulButton = el.querySelector('button#switchPlayers');
+    expect(breakingFoulButton).toBeNull();
+  }));
+
+  fit('hides force re-rack when any balls are made', async(() => {
+    component.game.endTurn(EndingType.BreakingFoul);
+    component.ballsRemaining = 14;
+    fixture.detectChanges();
+    const el: HTMLElement = fixture.nativeElement;
+    const breakingFoulButton = el.querySelector('button#forceRerack');
+    expect(breakingFoulButton).toBeNull();
+  }));
 });
