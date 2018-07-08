518ee791da5d3959f912a3e5f644b477afe6180b
Taylor, Robert <Robert.Taylor@wolterskluwer.com>
2018-07-08 11:39:26 -0400

    adding default service-worker

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { GameComponent } from './game/game.component';
 import { StraightPoolRulesService } from './straight-pool-rules.service';
 import { PlayersComponent } from './players/players.component';
 import { PlayerComponent } from './player/player.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { PlayerComponent } from './player/player.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [StraightPoolRulesService],
   bootstrap: [AppComponent]
