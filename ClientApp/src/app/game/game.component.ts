6ed7df82daf1f02eaee97a33d4ab3153cc1b8e45
Taylor, Robert <Robert.Taylor@wolterskluwer.com>
2018-07-14 01:17:35 -0400

    added Details tab

--- ClientApp/src/app/game/game.component.ts
+++ ClientApp/src/app/game/game.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { StraightPoolRulesService } from '../straight-pool-rules.service';
 import { StraightPoolGame } from '../straight-pool-game';
 import { EndingType } from '../straight-pool-ending-type.enum';
+import { StraightPoolTurn } from '../straight-pool-turn';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-game',
@@ -11,6 +13,7 @@ import { EndingType } from '../straight-pool-ending-type.enum';
 export class GameComponent implements OnInit {
   game: StraightPoolGame;
   ballsRemaining = 15;
+  obsTurns = new Subject<StraightPoolTurn[]>();
 
   constructor(public rules: StraightPoolRulesService) {
     this.game = rules.currentGame = rules.newGame();
@@ -22,6 +25,8 @@ export class GameComponent implements OnInit {
   private endTurn(ending: EndingType, ballsRemaining: number = this.ballsRemaining) {
     this.game.endTurn(ending, ballsRemaining);
     this.ballsRemaining = this.game.ballsRemaining;
+
+    this.obsTurns.next(this.game.turns);
   }
 
   miss() { this.endTurn(EndingType.Miss); }
